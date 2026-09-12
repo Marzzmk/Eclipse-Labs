3ee06dd3db31cce69dbb58668088aff149245917
unknown <marzzitoz@gmail.com>
2026-09-12 09:46:41 -0500

    add:funcion de servicios en server

--- servidor.js
+++ servidor.js
@@ -1,4 +1,29 @@
 const http = require("http");
+// Array de servicios
+
+const servicios = [
+    {
+        nombre: "Creacion de paginas web",
+        descripcion: "Creamos paginas web para tu negocio, emprendimiento o trabajo"
+    },
+    {
+        nombre: "Creacion de juegos",
+        descripcion: "Creamos un juego personalizado para ti"
+    
+    }
+];
+
+function mostrarServicios () {
+    return servicios.map(servicio => `
+        <div>
+        <h1>${servicio.nombre}</h1>
+        <p>${servicio.descripcion}</p>
+        </div>
+        `).join("");
+
+}
+
+
 const servidor = http.createServer((req, res) => {
     if (req.url === "/") {
     res.writeHead(200, {"Content-type": "text/plain" });
@@ -6,11 +31,17 @@ const servidor = http.createServer((req, res) => {
     } else if (req.url === "/nosotros") {
         res.writeHead(200, {"content-type": "text/html"});
         res.end("<h1>Sobre nosotros</h1>")
-    }  else {
+    } else if (req.url === "/servicios"){
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(`
+            <h1>Nuestros servicios</h1>
+            ${mostrarServicios()}
+            `);  
+    } else {
         res.writeHead(404, {"Content-Type": "text/html"});
         res.end("<h1>Pagina no encontrada</h1>");
     }
-});
+    });
 
 servidor.listen(3000, () => {
     console.log ("Servidor corriendo en http://localhost:3000");
